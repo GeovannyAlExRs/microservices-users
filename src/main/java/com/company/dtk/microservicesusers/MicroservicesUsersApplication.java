6a22ee8c749fd7c12e3f86abcd89f0f51033991d
Geovanny AlEx Rs <axel94bsc@gmail.com>
2023-05-18 16:28:57 -0500

    Initial Commit Structure Project

---
+++ src/main/java/com/company/dtk/microservicesusers/MicroservicesUsersApplication.java
@@ -0,0 +1,13 @@
+package com.company.dtk.microservicesusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/dtk/microservicesusers/MicroservicesUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.dtk.microservicesusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
